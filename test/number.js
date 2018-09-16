3df0664438eb5d7cc06dbcfa7b8bd4e1e1099c32
entrptaher@gmail.com <entrptaher@gmail.com>
2018-09-16 21:50:36 +0600

    Change test structure

--- test/number.js
+++ test/number.js
@@ -2,12 +2,17 @@ const { expect } = require("chai");
 const app = require("../app");
 
 describe("tests example website", function() {
-  it("title has text: example", async function() {
-    const { title } = await app();
-    expect(title.toLowerCase()).to.have.string("example");
+  it("browses the website", async function() {
+    const { title, outerHTML } = await app();
+    this.title = title;
+    this.outerHTML = outerHTML;
   });
+
+  it("title has text: example", async function() {
+    expect(this.title.toLowerCase()).to.have.string("example");
+  });
+
   it("outerHTML has text: example", async function() {
-    const { outerHTML } = await app();
-    expect(outerHTML.toLowerCase()).to.have.string("example");
+    expect(this.outerHTML.toLowerCase()).to.have.string("example");
   });
 });
