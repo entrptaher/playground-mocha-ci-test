2d3795d3c516c847e00613f0f7baf8493f0e444f
entrptaher@gmail.com <entrptaher@gmail.com>
2018-09-16 22:12:39 +0600

    revert Headless:true

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 module.exports = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
   const page = await browser.newPage();
